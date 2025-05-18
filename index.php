ef5a8191a7d3e078cc61b75b916c548b88ffe11b
pnytrainings123-pny <hsuntariq@gmail.com>
2025-05-18 14:03:39 +0500

    navbar started

---
+++ index.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include './bootstrap.php' ?>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <title>Home - WoodMard Accessories</title>
+</head>
+
+<body>
+
+    <div class="col-xxl-9 col-xl-11 mx-auto p-4">
+        <div class="d-flex justify-content-between align-items-center">
+            <div class="d-flex align-items-center gap-3 gap-xl-5">
+                <img width="180px" src="./assets/icons/wood-logo-dark.svg" alt="logo">
+                <ul class="d-none d-xl-flex m-0 align-items-center gap-4 list-unstyled">
+                    <li class="position-relative nav-li">
+                        <div class="d-flex">Cases <img src="./assets/icons/dropdown.svg" width="10px" alt=""> </div>
+                        <ul style="width: 200px;top:40px;"
+                            class="card nested-list position-absolute text-secondary list-unstyled p-3 shadow">
+                            <li class="py-1">iPhone 13</li>
+                            <li class="py-1">iPhone 12 pro</li>
+                            <li class="py-1">iPhone 12 </li>
+                            <li class="py-1">iPhone 11</li>
+                            <li class="py-1">iPhone 11 pro </li>
+                            <li class="py-1">iPhone SE</li>
+                            <li class="py-1">iPhone XR</li>
+                        </ul>
+                    </li>
+                    <li class="position-relative nav-li">
+                        <div class="d-flex">Straps <img src="./assets/icons/dropdown.svg" width="10px" alt=""></div>
+                        <ul style="width: 200px;top:40px;"
+                            class="card nested-list position-absolute text-secondary list-unstyled p-3 shadow">
+                            <li class="py-1">iPhone 13</li>
+                            <li class="py-1">iPhone 12 pro</li>
+                            <li class="py-1">iPhone 12 </li>
+                            <li class="py-1">iPhone 11</li>
+                            <li class="py-1">iPhone 11 pro </li>
+                            <li class="py-1">iPhone SE</li>
+                            <li class="py-1">iPhone XR</li>
+                        </ul>
+                    </li>
+                    <li class="position-relative nav-li">
+                        <div class="d-flex">Power Bank <img src="./assets/icons/dropdown.svg" width="10px" alt=""></div>
+                        <ul style="width: 200px;top:40px;"
+                            class="card nested-list position-absolute text-secondary list-unstyled p-3 shadow">
+                            <li class="py-1">iPhone 13</li>
+                            <li class="py-1">iPhone 12 pro</li>
+                            <li class="py-1">iPhone 12 </li>
+                            <li class="py-1">iPhone 11</li>
+                            <li class="py-1">iPhone 11 pro </li>
+                            <li class="py-1">iPhone SE</li>
+                            <li class="py-1">iPhone XR</li>
+                        </ul>
+                    </li>
+                    <li class="position-relative nav-li">
+                        <div class="d-flex">Cabels <img src="./assets/icons/dropdown.svg" width="10px" alt=""></div>
+                        <ul style="width: 200px;top:40px;"
+                            class="card nested-list position-absolute text-secondary list-unstyled p-3 shadow">
+                            <li class="py-1">iPhone 13</li>
+                            <li class="py-1">iPhone 12 pro</li>
+                            <li class="py-1">iPhone 12 </li>
+                            <li class="py-1">iPhone 11</li>
+                            <li class="py-1">iPhone 11 pro </li>
+                            <li class="py-1">iPhone SE</li>
+                            <li class="py-1">iPhone XR</li>
+                        </ul>
+                    </li>
+                    <li class="position-relative nav-li">
+                        <div class="d-flex">MagSafe <img src="./assets/icons/dropdown.svg" width="10px" alt=""></div>
+                        <ul style="width: 200px;top:40px;"
+                            class="card nested-list position-absolute text-secondary list-unstyled p-3 shadow">
+                            <li class="py-1">iPhone 13</li>
+                            <li class="py-1">iPhone 12 pro</li>
+                            <li class="py-1">iPhone 12 </li>
+                            <li class="py-1">iPhone 11</li>
+                            <li class="py-1">iPhone 11 pro </li>
+                            <li class="py-1">iPhone SE</li>
+                            <li class="py-1">iPhone XR</li>
+                        </ul>
+                    </li>
+                    <li class="position-relative nav-li">
+                        <div class="d-flex">Charger <img src="./assets/icons/dropdown.svg" width="10px" alt=""></div>
+                        <ul style="width: 200px;top:40px;"
+                            class="card nested-list position-absolute text-secondary list-unstyled p-3 shadow">
+                            <li class="py-1">iPhone 13</li>
+                            <li class="py-1">iPhone 12 pro</li>
+                            <li class="py-1">iPhone 12 </li>
+                            <li class="py-1">iPhone 11</li>
+                            <li class="py-1">iPhone 11 pro </li>
+                            <li class="py-1">iPhone SE</li>
+                            <li class="py-1">iPhone XR</li>
+                        </ul>
+                    </li>
+                    <li class="position-relative nav-li">
+                        <div class="d-flex">More <img src="./assets/icons/dropdown.svg" width="10px" alt=""></div>
+                        <ul style="width: 200px;top:40px;"
+                            class="card nested-list position-absolute text-secondary list-unstyled p-3 shadow">
+                            <li class="py-1">iPhone 13</li>
+                            <li class="py-1">iPhone 12 pro</li>
+                            <li class="py-1">iPhone 12 </li>
+                            <li class="py-1">iPhone 11</li>
+                            <li class="py-1">iPhone 11 pro </li>
+                            <li class="py-1">iPhone SE</li>
+                            <li class="py-1">iPhone XR</li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <div class="d-flex gap-3 align-items-center">
+                <p class="m-0 fw-semibold text-secondary-emphasis">Login/Register</p>
+                <div class="d-flex gap-3 fs-5">
+                    <i class="bi bi-search"></i>
+                    <div class="position-relative">
+                        <div class="rounded-circle position-absolute  text-xsm d-flex justify-content-center align-items-center text-white fw-bold"
+                            style="height: 12px;width:12px;background:#2E6BC6;right:-4px">
+                            0
+                        </div>
+                        <i class="fw-semibold text-secondary-emphasis bi bi-shuffle"></i>
+                    </div>
+                    <div class="position-relative">
+                        <div class="position-relative">
+                            <div class="rounded-circle position-absolute  text-xsm d-flex justify-content-center align-items-center text-white fw-bold"
+                                style="height: 12px;width:12px;background:#2E6BC6;right:-4px">
+                                0
+                            </div>
+                            <i class="fw-semibold text-secondary-emphasis bi bi-heart"></i>
+                        </div>
+                    </div>
+                    <div class="position-relative">
+                        <div class="position-relative">
+                            <div class="rounded-circle position-absolute  text-xsm d-flex justify-content-center align-items-center text-white fw-bold"
+                                style="height: 12px;width:12px;background:#2E6BC6;right:-4px">
+                                0
+                            </div>
+                            <i class="fw-semibold text-secondary-emphasis bi bi-cart"></i>
+                        </div>
+                    </div>
+
+                </div>
+                <p class="m-0 fw-semibold text-secondary-emphasis">$0.00</p>
+            </div>
+        </div>
+    </div>
+
+
+</body>
+
+</html>
