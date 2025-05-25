b844fe196562288f1755f55ff824ce526364c799
pnytrainings123-pny <hsuntariq@gmail.com>
2025-05-25 14:01:35 +0500

    slider

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 let opener = document.querySelector('.open-sidebar')
 let sidebar = document.querySelector('.sidebar')
 let sidebar_menu = document.querySelector('.sidebar-menu')
+let slider = document.getElementById('slider');
 
 let hero_para = document.querySelector('.hero-para')
 let hero_heading = document.querySelector('.hero-heading')
-let hero_image = document.querySelector('.hero-image')
+let hero_content = document.querySelectorAll('.hero-content')
+let hero_image = document.querySelectorAll('.hero-image')
 let next = document.querySelector('.forward')
 let prev = document.querySelector('.backward')
 
@@ -25,41 +27,85 @@ sidebar_menu.addEventListener('click', (e) => {
 })
 
 
-const data = [
-    {
-        heading: 'For Everything and Everyone',
-        para: 'Even if your less into design and more into content strategy you may find some redeeming value with, wait for it, dummy copy.',
-        image: './assets/slider-images/1.jpg'
-    },
-    {
-        heading: 'Featured Accessories',
-        para: 'A client that’s unhappy for a reason is a problem, a client that’s unhappy though required he or her can’t quite put a finger.',
-        image: './assets/slider-images/2.jpg'
-    },
-    {
-        heading: 'Charge Your Phone Safely!',
-        para: 'A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.',
-        image: './assets/slider-images/3.jpg'
-    },
-]
-let count = 0
-next.addEventListener('click', () => {
-    count++
-    if (count > 2) {
-        count = 0
-    }
-    hero_image.style.backgroundImage = `url(${data[count].image})`
-    hero_heading.innerHTML = data[count].heading
-    hero_para.innerHTML = data[count].para
+// const data = [
+//     {
+//         heading: 'For Everything and Everyone',
+//         para: 'Even if your less into design and more into content strategy you may find some redeeming value with, wait for it, dummy copy.',
+//         image: './assets/slider-images/1.jpg'
+//     },
+//     {
+//         heading: 'Featured Accessories',
+//         para: 'A client that’s unhappy for a reason is a problem, a client that’s unhappy though required he or her can’t quite put a finger.',
+//         image: './assets/slider-images/2.jpg'
+//     },
+//     {
+//         heading: 'Charge Your Phone Safely!',
+//         para: 'A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.',
+//         image: './assets/slider-images/3.jpg'
+//     },
+// ]
+// let count = 0
+// next.addEventListener('click', () => {
+//     count++
+//     if (count > 2) {
+//         count = 0
+//     }
+//     hero_image.style.backgroundImage = `url(${data[count].image})`
+//     hero_heading.innerHTML = data[count].heading
+//     hero_para.innerHTML = data[count].para
+// })
+
+
+// prev.addEventListener('click', () => {
+//     count--
+//     if (count < 0) {
+//         count = 2
+//     }
+//     hero_image.style.backgroundImage = `url(${data[count].image})`
+//     hero_heading.innerHTML = data[count].heading
+//     hero_para.innerHTML = data[count].para
+// })
+
+
+
+window.addEventListener('load', () => {
+    hero_content[0].classList.add('move-content')
 })
 
+
+
+let count = 0;
+
+next.addEventListener('click', () => {
+    count++;
+    if (count > 2) {
+        count = 0;
+    }
+
+    hero_image.forEach((item) => {
+        item.style.transform = `translateX(${count * -100}%)`;
+    });
+
+    hero_content.forEach((content) => {
+        content.classList.remove('move-content');
+    });
+
+    hero_content[count].classList.add('move-content');
+});
 
 prev.addEventListener('click', () => {
-    count--
+    count--;
     if (count < 0) {
-        count = 2
+        count = 2;
     }
-    hero_image.style.backgroundImage = `url(${data[count].image})`
-    hero_heading.innerHTML = data[count].heading
-    hero_para.innerHTML = data[count].para
-})
+
+    hero_image.forEach((item) => {
+        item.style.transform = `translateX(${count * -100}%)`;
+    });
+
+    hero_content.forEach((content) => {
+        content.classList.remove('move-content');
+    });
+
+    hero_content[count].classList.add('move-content');
+});
--- slider.php
+++ slider.php
@@ -1,8 +1,9 @@
-<div class="w-100 position-relative" style="height: 70vh;">
-    <i class="bi bi-chevron-right end-0 me-4 top-50 forward position-absolute fs-2" style="cursor: pointer;"></i>
-    <i class="bi bi-chevron-left top-50 ms-4 backward position-absolute fs-2" style="cursor: pointer;"></i>
-    <div class="hero-image"
-        style="background-image:url('./assets/slider-images/1.jpg');height:100%;background-size:cover;background-position:center center;">
+<div class="d-flex overflow-hidden position-relative" style="height: 70vh;width:100vw">
+    <i class="bi bi-chevron-right end-0 me-4 top-50 forward position-absolute fs-2"
+        style="cursor: pointer;z-index:999"></i>
+    <i class="bi bi-chevron-left top-50 ms-4 backward position-absolute fs-2" style="cursor: pointer;z-index:999"></i>
+    <div class="hero-image flex-shrink-0 "
+        style="background-image:url('./assets/slider-images/1.jpg');height:100%;background-size:cover;background-position:center center; width:100vw">
         <div
             class="col-xxl-9  col-xl-11 mx-auto p-4 text-center text-md-start hero-content p-xl-5 d-flex flex-column justify-content-center  h-100 p-3">
             <h1 class="display-3 fw-bold hero-heading col-xl-6 col-md-6 col-sm-9 col-11">For Everything and Everyone
@@ -13,6 +14,52 @@
                 some
                 redeeming value with, wait
                 for it, dummy copy.</p>
+            <div class="d-flex gap-3 bg-transparent  justify-content-center justify-content-md-start">
+                <button style="background:#2E6BC6;font-size:0.8rem"
+                    class="btn  fw-bold text-white py-2 rounded-1 text-uppercase">
+                    to shop
+                </button>
+                <button style="font-size:0.8rem;"
+                    class="btn border  fw-bold border-2 text-secondary-emphasis py-2 rounded-1 text-uppercase">
+                    read more
+                </button>
+            </div>
+        </div>
+
+    </div>
+
+    <!-- second -->
+    <div class="hero-image flex-shrink-0 "
+        style="background-image:url('./assets/slider-images/2.jpg');height:100%;background-size:cover;background-position:center center; width:100vw">
+        <div
+            class="col-xxl-9  col-xl-11 mx-auto p-4 text-center text-md-start hero-content p-xl-5 d-flex flex-column justify-content-center  h-100 p-3">
+            <h1 class="display-3 fw-bold hero-heading col-xl-6 col-md-6 col-sm-9 col-11">Featured Accessories
+            </h1>
+            <p class="text-secondary  col-xl-4 hero-para col-md-5 col-sm-9 col-11">A client that’s unhappy for a reason
+                is a problem, a client that’s unhappy though required he or her can’t quite put a finger.</p>
+            <div class="d-flex gap-3 bg-transparent  justify-content-center justify-content-md-start">
+                <button style="background:#2E6BC6;font-size:0.8rem"
+                    class="btn  fw-bold text-white py-2 rounded-1 text-uppercase">
+                    to shop
+                </button>
+                <button style="font-size:0.8rem;"
+                    class="btn border  fw-bold border-2 text-secondary-emphasis py-2 rounded-1 text-uppercase">
+                    read more
+                </button>
+            </div>
+        </div>
+
+    </div>
+
+    <!-- third -->
+    <div class="hero-image flex-shrink-0 "
+        style="background-image:url('./assets/slider-images/3.jpg');height:100%;background-size:cover;background-position:center center; width:100vw">
+        <div
+            class="col-xxl-9  col-xl-11 mx-auto p-4 text-center text-md-start hero-content p-xl-5 d-flex flex-column justify-content-center  h-100 p-3">
+            <h1 class="display-3 fw-bold hero-heading col-xl-6 col-md-6 col-sm-9 col-11">Charge Your Phone Safely!
+            </h1>
+            <p class="text-secondary  col-xl-4 hero-para col-md-5 col-sm-9 col-11">A wonderful serenity has taken
+                possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</p>
             <div class="d-flex gap-3 bg-transparent  justify-content-center justify-content-md-start">
                 <button style="background:#2E6BC6;font-size:0.8rem"
                     class="btn  fw-bold text-white py-2 rounded-1 text-uppercase">
